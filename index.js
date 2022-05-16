1579638f90e0d2657f9c93f2fcad76260bedf55c
Thao. Quan Ngoc - CMC Global DU1.19 <qnthao@cmcglobal.vn>
2022-05-16 13:58:16 +0700

    1 change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.json({
     success: true,
-    data: [{ value: "Hello World!" }],
+    data: [{ value: "Hello World! Changed me" }],
   });
 });
 
